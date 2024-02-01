d8ae5c8ef99c217860c1ab7bc05fe4896b250df9
Papiya Mazumder <papiyamazumder2@gmail.com>
2024-02-01 21:21:42 +0530

    Add files via upload

---
+++ Csharp_CodingTest/Csharp_CodingTest/Program.cs
@@ -0,0 +1,96 @@
+﻿//internal class Program        // STUDENT CLASS 
+//{
+//    private static void Main(string[] args)
+//    {
+//        Console.Write("Enter student name: ");
+//        string inputName = Console.ReadLine().ToUpper();
+
+//        List<Student> students = new List<Student>
+//            {
+//                new Student(111, "A", "C#"),
+//                new Student(112, "B", "ASP.NET"),
+//                new Student(97, "C", "C#"),
+//                new Student(87, "D", "C#")
+//            };
+
+//        Student foundStudent = students.Find(student => student.Name == inputName);
+
+//        if (foundStudent != null)
+//        {
+//            Console.Write("Enter Amount Paid: ");
+//            var amt = Convert.ToInt32(Console.ReadLine().ToUpper());
+
+//            foundStudent.Payment(amt);
+//            Console.WriteLine("");
+//            Console.WriteLine("Student Details: ");
+//            foundStudent.Print();
+//        }
+//        else
+//        {
+//            Console.WriteLine($"Student with name {inputName} not found in the list.");
+//        }
+//    }
+//}
+
+//public class Student
+//{
+//    public int RollNo { get; }
+//    public string Name { get; }
+//    public string Course { get; }
+//    public int FeePaid { get; set; }
+//    public int DueAmount => TotalFee - FeePaid; 
+//    public int TotalFee { get; set; }
+
+//    private const int FeeForCSharp = 2000;
+//    private const int FeeForAspNet = 3000;
+
+//    public Student (int rollNo, string name, string course)
+//    {
+//        RollNo = rollNo;
+//        Name = name;
+//        Course = course;
+
+//        FeePaid = 0;
+
+//        if (Course == "C#")
+//        {
+//            TotalFee = FeeForCSharp;
+//        }
+//        else if (Course == "ASP.NET")
+//        {
+//            TotalFee = FeeForAspNet;
+//        }
+//        else
+//        {
+//            TotalFee = 0;
+//        }
+//        // OR
+//        //TotalFee = (course == "C#") ? FeeForCSharp : (course == "ASP.NET") ? FeeForAspNet : 0;
+
+//        //DueAmount = TotalFee - FeePaid;
+//    }
+
+//    public void Payment(int amount)
+//    {
+//        if (amount < 0)
+//        {
+//            Console.WriteLine("Invalid payment amount. Please enter the amount of course fee paid.");
+//        }
+//        else
+//        {
+//            FeePaid = FeePaid + amount;
+//            Console.WriteLine($"Payment of {amount} made successfully. Fee Due for {Course} course : {DueAmount} ");
+//        }
+//    }
+
+//    public void Print()
+//    {
+//        Console.WriteLine($"Roll No: {RollNo}");
+//        Console.WriteLine($"Name: {Name}");
+//        Console.WriteLine($"Course: {Course}");
+//        Console.WriteLine($"Fee Paid: {FeePaid}");
+//        Console.WriteLine($"Due Amount: {DueAmount}");
+//        Console.WriteLine($"Total Fee: {TotalFee}");
+//    }
+
+//}
---
+++ Csharp_CodingTest/Csharp_CodingTest/Program2.cs
@@ -0,0 +1,105 @@
+﻿//using System;       // STUDENT AND PERSON
+
+//class Person
+//{
+//    public string firstName;
+//    public string lastName;
+//    public int id;
+
+//    public Person(string firstName, string lastName, int id)
+//    {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.id = id;  
+//    }
+//}
+
+//class Student : Person
+//{
+//    public int[] testScores;
+
+//    public Student(string firstName, string lastName, int id, int[] testScores) : base(firstName, lastName, id)
+//    {
+//        this.testScores = testScores;  
+//    }
+
+//    public char calculate()
+//    {
+//        //int sum = 0;
+//        //foreach (var test in testScores)
+//        //{
+//        //    test += sum;
+//        //}
+//        // OR
+//        int sum = testScores.Sum();  // Assuming you have a using directive for System.Linq
+//        int average = sum / testScores.Length;
+//        Console.WriteLine($"Average: {average}");
+
+//        if (average >= 90 && average <= 100)
+//        {
+//            return 'O';
+//        }
+//        else if (average >= 80 && average < 90)
+//        {
+//            return 'E';
+//        }
+//        else if (average >= 70 && average < 80)
+//        {
+//            return 'A';
+//        }
+//        else if (average >= 55 && average < 70)
+//        {
+//            return 'P';
+//        }
+//        else if (average >= 40 && average < 55)
+//        {
+//            return 'D';
+//        }
+//        else if (average < 40 && average > 0)
+//        {
+//            return 'T';
+//        }
+//        else
+//        {
+//            return ' ';
+//        }
+//    }
+//}
+
+//class Solution
+//{
+//    static void Main()
+//    {
+//        //int[] testScores = { 100, 80 };
+//        //Student s = new Student("Heraldo", "Memelli", 8135627, testScores);
+//        //var grade = s.calculate();
+
+//        //Console.WriteLine($"Name: {s.lastName}, {s.firstName}\nID: {s.id}");
+//        //Console.WriteLine($"Grade: {grade}");
+
+//        // OR taking user input:
+
+//        Console.WriteLine("Enter firstname and last name: ");
+//        var FN = Console.ReadLine().ToUpper();
+//        var LN = Console.ReadLine().ToUpper();
+//        Console.Write("Enter your ID: ");
+//        var ID = Console.ReadLine();
+
+//        if (FN.Length >= 1 && FN.Length <= 10 && LN.Length >= 1 && LN.Length <= 10 && ID.Length == 7)
+//        {
+//            Console.Write("Enter test scores (space-separated): ");
+//            string scoresInput = Console.ReadLine();
+//            int[] testScores = Array.ConvertAll(scoresInput.Split(' '), int.Parse);
+
+//            Student s = new Student(FN, LN, int.Parse(ID), testScores);
+//            Console.WriteLine("\nStudent Details: ");
+//            Console.Write($"Name: {s.lastName}, {s.firstName}\nID: {s.id} \n");
+//            string grade = s.calculate().ToString();
+//            Console.Write($"Grade: {grade}");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Invalid input. First name and last name should be between 1 and 10 characters each, and ID should be 7 characters.");
+//        }
+//    }
+//}
